935fdea120381aa64b8944c954c0b0e055727836
Ayse Erdem <104596061+ayseerdem06@users.noreply.github.com>
2022-08-16 22:11:02 +0300

    Update Day1Git.java

--- src/Day1Git.java
+++ src/Day1Git.java
@@ -3,6 +3,7 @@ public class Day1Git {
         System.out.println("Hello Git 1.Deneme");
         System.out.println("Hello git 3.deneme");
         System.out.println("Hello git 4.deneme");
-         System.out.println("Hello git 5.deneme");
+        System.out.println("Hello git 5.deneme");
+        System.out.println("Hello git 5.deneme");
     }
 }
