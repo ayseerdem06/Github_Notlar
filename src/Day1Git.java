e43b36b68970bde75f512a14aa2698758c7a9bba
Ayşe Erdem <aayseerdem0633@gmail.com>
2022-08-16 22:06:35 +0300

    cvc

--- src/Day1Git.java
+++ src/Day1Git.java
@@ -2,5 +2,6 @@ public class Day1Git {
     public static void main(String[] args) {
         System.out.println("Hello Git 1.Deneme");
         System.out.println("Hello git 3.deneme");
+        System.out.println("Hello git 4.deneme");
     }
 }
